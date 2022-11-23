fe0a13a63619c2bcc1be23e76184a4192f65f17f
GutjahrT <theo.gutjahr@ovgu.de>
2022-11-23 21:56:40 +0100

    Initial commit

---
+++ src/test/kotlin/com/gutjahr/SpringBootServiceStuff/SpringBootServiceStuffApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gutjahr.SpringBootServiceStuff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootServiceStuffApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
